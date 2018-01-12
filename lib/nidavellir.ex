f2d31550459ff21961038e6302a19623db8e05cf
Gabriel Malaquias de Souza <gabriel.malaquias@shore.com>
2018-01-12 11:55:08 +0100

    Add redix properly

--- lib/nidavellir.ex
+++ lib/nidavellir.ex
@@ -1,4 +1,6 @@
 defmodule Nidavellir do
+  use Application
+
   @moduledoc """
   Documentation for Nidavellir.
   """
@@ -14,5 +16,16 @@ defmodule Nidavellir do
   """
   def hello do
     :world
+  end
+
+  def start(_type, _args) do
+    import Supervisor.Spec, warn: false
+
+    children = [
+      worker(Redix, [[], [name: :redix]])
+    ]
+
+    opts = [strategy: :one_for_one, name: Nidavellir.RedixSupervisor]
+    Supervisor.start_link(children, opts)
   end
 end
--- mix.exs
+++ mix.exs
@@ -4,16 +4,20 @@ defmodule Nidavellir.Mixfile do
   def project do
     [app: :nidavellir,
      version: "0.1.0",
-     elixir: "~> 1.6-dev",
+     elixir: "~> 1.5.3",
      start_permanent: Mix.env == :prod,
      deps: deps()]
   end
 
   def application do
-    [extra_applications: [:logger, :redix]]
+    [extra_applications: [:logger, :redix],
+    mod: { Nidavellir, []}]
   end
 
   defp deps do
-     [{:redix, ">= 0.0.0"}]
+    [
+      {:redix, ">= 0.0.0"},
+      {:hulaaki, "~> 0.1.1"}
+    ]
   end
 end
